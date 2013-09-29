ca92d2b910002e32b64ea697b7fb75d4f6875fd7
mbrechet <m.brechet@gmail.com>
2013-09-29 11:03:06 +0200

    initial Commit

---
+++ HourCounter/src/main/java/com/mbr/hourcounter/MainPage.java
@@ -0,0 +1,19 @@
+package com.mbr.hourcounter;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+/**
+ * Created by fnzl2784 on 28/09/13.
+ */
+public class MainPage extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.xml.main_layout);
+    }
+    public void onStart(){
+        Button trackHour;
+    }
+}
